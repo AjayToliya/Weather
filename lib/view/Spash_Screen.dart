df484f14b591f3a3d7405235cc590736c06a12fd
AjayToliya <toliyaajay2212@gmail.com>
2024-08-12 11:37:52 +0530

    second

--- lib/view/Spash_Screen.dart
+++ lib/view/Spash_Screen.dart
@@ -11,34 +11,13 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> fadeAnimation;
-
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    fadeAnimation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeIn,
-    );
-
-    controller.forward();
-
     Timer(Duration(seconds: 3), () {
       Navigator.of(context).pushReplacementNamed('Home');
     });
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 
   @override
@@ -48,24 +27,21 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FadeTransition(
-              opacity: fadeAnimation,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.network(
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzK5Ogcj8Ury2tR_4Gv3EskaAtC0EA1LCLAg&s",
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.cover,
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzK5Ogcj8Ury2tR_4Gv3EskaAtC0EA1LCLAg&s",
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
             const SizedBox(height: 20),
             const Text(
-              "How The Day???",
+              "How's The Day???",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
